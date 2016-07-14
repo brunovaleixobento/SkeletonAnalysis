a823d533b569990800337f7ec19cefb75b2ac7ea
bruno <brucms16@lip.pt>
2016-07-14 17:22:12 +0100

    segundo grafico no plotVar.C

--- macros/plotVar.C
+++ macros/plotVar.C
@@ -3,7 +3,10 @@
 #include "TROOT.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TLine.h"
+#include "TStyle.h"
 #include "TH1D.h"
+#include "TGraphErrors.h"
 #include "TCanvas.h"
 #include "TChain.h"
 #include "THStack.h"
@@ -19,7 +22,7 @@ class variable{
 
 public:
 
-  variable(string name="", string expression = "", int bins=30, double xmin=0, double xmax=100, string leg = "");
+  variable(string name="", string expression = "", int bins=30, double xmin=0, double xmax=100, string leg = "", int Logy=0);
 
   string GetName() {return fname;}
   string GetExpression() {return fexpression;}
@@ -27,6 +30,7 @@ public:
   double GetXMin() {return fxmin;}
   double GetXMax() {return fxmax;}
   string GetLeg() {return fleg;}
+  int GetLogy() {return fLogy;}
 
   void SetName(string name) {fname=name;}
   void SetExpression(string expression) {fexpression=expression;}
@@ -43,41 +47,42 @@ private:
   double fxmin;
   double fxmax;
   string fleg;
+  int fLogy;
 };
 
-variable::variable(string name, string expression, int bins, double xmin, double xmax, string leg): fname(name), fexpression(expression), fbins(bins), fxmin(xmin), fxmax(xmax), fleg(leg){}
+variable::variable(string name, string expression, int bins, double xmin, double xmax, string leg, int Logy): fname(name), fexpression(expression), fbins(bins), fxmin(xmin), fxmax(xmax), fleg(leg), fLogy(Logy){}
 
 int plotVar()
 {
   vector<variable> vvariable;
 
-  variable LepPt("LepPt","LepPt",30,0,200,"p_{T} (l) [GeV]");
-  variable LepEta("LepEta","LepEta",30,-3,3,"#eta (l)");
-  variable Njet("Njet","Njet",11,-0.5,10.5,"Njet");
-  variable Jet1Pt("Jet1Pt","Jet1Pt",30,100,500,"p_{T} (Jet1) [GeV]");
-  variable Jet1Eta("Jet1Eta","Jet1Eta",30,-3,3,"Eta (Jet1)");
-  variable Met("Met","Met",30,150,500,"Met [GeV]");
-  variable CosDPhi("CosDeltaPhi","CosDeltaPhi",30,-1.2,1.2,"Cos(#Delta #Phi)");
-  variable DrJet1Lep("DrJet1Lep","DrJet1Lep",30,0,6,"Dr Jet1 Lep");
-  variable DrJet2Lep("DrJet2Lep","DrJet2Lep",30,0,6,"Dr Jet2 Lep");
-  variable Jet2Pt("Jet2Pt","Jet2Pt",30,0,400,"p_{T} (Jet2) [GeV]");
-  variable mt("mt","mt",30,0,250,"mt [GeV]");
-  variable HT20("HT20","HT20",30,0,2000,"HT20 [GeV]");
+  variable LepPt("LepPt","LepPt",30,0,200,"p_{T} (l) [GeV]",0);
+  variable LepEta("LepEta","LepEta",30,-3,3,"#eta (l)",0);
+  variable Njet("Njet","Njet",11,-0.5,10.5,"Njet",0);
+  variable Jet1Pt("Jet1Pt","Jet1Pt",30,100,500,"p_{T} (Jet1) [GeV]",0);
+  variable Jet1Eta("Jet1Eta","Jet1Eta",30,-3,3,"Eta (Jet1)",0);
+  variable Met("Met","Met",30,300,600,"Met [GeV]",1);
+  variable CosDPhi("CosDeltaPhi","CosDeltaPhi",30,-1.2,1.2,"Cos(#Delta #Phi)",0);
+  variable DrJet1Lep("DrJet1Lep","DrJet1Lep",30,0,6,"Dr Jet1 Lep",0);
+  variable DrJet2Lep("DrJet2Lep","DrJet2Lep",30,0,6,"Dr Jet2 Lep",0);
+  variable Jet2Pt("Jet2Pt","Jet2Pt",30,0,400,"p_{T} (Jet2) [GeV]",0);
+  variable mt("mt","mt",30,0,250,"mt [GeV]",0);
+  variable HT20("HT20","HT20",30,0,2000,"HT20 [GeV]",0);
   variable HT30("HT30","HT30",30,0,2000,"HT30 [GeV]");
 
-  vvariable.push_back(LepPt);
-  vvariable.push_back(LepEta);
-  vvariable.push_back(Njet);
-  vvariable.push_back(Jet1Pt);
-  /*vvariable.push_back(Jet1Eta);
+  //vvariable.push_back(LepPt);
+  //vvariable.push_back(LepEta);
+  //vvariable.push_back(Njet);
+  //vvariable.push_back(Jet1Pt);
+  //vvariable.push_back(Jet1Eta);
   vvariable.push_back(Met);
-  vvariable.push_back(CosDPhi);
-  vvariable.push_back(DrJet1Lep);
-  vvariable.push_back(DrJet2Lep);
-  vvariable.push_back(Jet2Pt);
-  vvariable.push_back(mt);
-  vvariable.push_back(HT20);
-  vvariable.push_back(HT30);*/
+  //vvariable.push_back(CosDPhi);
+  //vvariable.push_back(DrJet1Lep);
+  //vvariable.push_back(DrJet2Lep);
+  //vvariable.push_back(Jet2Pt);
+  //vvariable.push_back(mt);
+  //vvariable.push_back(HT20);
+  //vvariable.push_back(HT30);*/
 
   // Open input file(s)
   string basedirectory = "/lstore/cms/cbeiraod/Stop4Body/nTuples_v2016-07-12/";
@@ -101,7 +106,6 @@ int plotVar()
 
   TTree* dataTree = static_cast<TTree*>(dataFile->Get("bdttree"));
 
-
   // Create canvas
   TCanvas * c1 = new TCanvas("Stop","Stop", 800, 600);
   TCanvas * c2 = new TCanvas("variable","variable", 800, 600);
@@ -123,6 +127,9 @@ int plotVar()
     vector<TH1D*> wjetsH;
     vector<TH1D*> stopH;
     vector<TH1D*> dataH;
+    vector<TH1D*> backgroundH;
+    vector<TH1D*> ratioH;
+    vector<TH1D*> emptyH;
 
   // Plots
   for(int i=0;i<int(vvariable.size());i++)
@@ -131,13 +138,18 @@ int plotVar()
       string swjetsH =  "wjetsH"+std::to_string(i);
       string sstopH =  "stopH"+std::to_string(i);
       string sdataH = "dataH"+std::to_string(i);
+      string sbackgroundH = "backgroundH"+std::to_string(i);
+      string sratioH = "ratioH"+std::to_string(i);
+//      string semptyH = "empty"+std::to_string(i);
 
       // Create histogram(s)
       ttbarH.push_back(new TH1D(sttbarH.c_str(), "ttbar", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
       wjetsH.push_back(new TH1D(swjetsH.c_str(), "wjets", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
       stopH.push_back(new TH1D(sstopH.c_str(), "Signal", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
       dataH.push_back(new TH1D(sdataH.c_str(), "Data", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
-
+      backgroundH.push_back(new TH1D(sbackgroundH.c_str(), "", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
+      ratioH.push_back(new TH1D(sratioH.c_str(), "", vvariable[i].GetBins(), vvariable[i].GetXMin(), vvariable[i].GetXMax()));
+//      emptyH.push_back(new TH1D(semptyH.c_str(),"",vvariable[i].GetBins(),vvariable[i].GetXMin(), vvariable[i].GetXMax()));
 
       ttbarH[i]->SetFillColor(kGreen-7);
       ttbarH[i]->SetLineColor(kGreen-7);
@@ -155,6 +167,36 @@ int plotVar()
       else
 	      c1->cd();
 
+      // Divide Pads in Canvas c1
+
+      double xlow, xup, ylow, yup;
+      gPad->GetPadPar(xlow, ylow, xup, yup);
+
+      double xlow1, xup1, ylow1, yup1;
+      double xlow2, xup2, ylow2, yup2;
+
+      xup1 = xup;
+      yup1 = yup;
+      xlow1 = xlow;
+      ylow1 = 0.2*yup;
+
+      xup2 = xup;
+      yup2 = ylow1;
+      xlow2 = xlow;
+      ylow2 = ylow+0.05*(yup-ylow);
+
+      gPad->Divide(1,2);
+      gPad->cd(2);
+      gPad->SetPad(xlow2,ylow2,xup2,yup2);
+
+      if(vvariable.size()!=1)
+	      c1->cd(i+1);
+      else
+	      c1->cd();
+
+      gPad->cd(1);
+      gPad->SetPad(xlow1,ylow1,xup1,yup1);
+
       //Create TCuts
       TCut muon = "(abs(LepID)==13)&&(LepIso03<0.2)";
       TCut electron = "(abs(LepID)==11)&&(LepIso03<0.2)";
@@ -166,7 +208,7 @@ int plotVar()
       //TCut ht20 = "HT20 > 450";
       //TCut mt = "mt < 70";
 
-      // Fill histogram(s)
+      // Fill histogram(s) signal & BACKGROUND & DATA
       ttbarTree->Draw((vvariable[i].GetExpression()+">>"+sttbarH).c_str(),"XS*5000/Nevt"*(emu&&ISRjet&&met),"goff");
       wjetsTree->Draw((vvariable[i].GetExpression()+">>"+swjetsH).c_str(),"XS*5000/Nevt"*(emu&&ISRjet&&met),"goff");
       stopTree->Draw((vvariable[i].GetExpression()+">>"+sstopH).c_str(),"XS*5000/Nevt"*(emu&&ISRjet&&met),"goff");  //MULTIPLICAR O SINAL
@@ -175,6 +217,22 @@ int plotVar()
       THStack *Stack = new THStack(vvariable[i].GetName().c_str(), (vvariable[i].GetName()+";"+vvariable[i].GetLeg().c_str()+";Evt.").c_str());
       Stack->Add(ttbarH[i]);
       Stack->Add(wjetsH[i]);
+
+      gPad->SetLogy(vvariable[i].GetLogy());
+
+      backgroundH[i]->Add(ttbarH[i],wjetsH[i]);
+      ratioH[i]->Divide(dataH[i],backgroundH[i]);
+      ratioH[i]->Sumw2();
+
+      for(int j=0; j< backgroundH[i]->GetNbinsX()+1; j++)
+      {
+        backgroundH[i]->SetBinError(j, backgroundH[i]->GetBinError(j)/backgroundH[i]->GetBinContent(j));
+        backgroundH[i]->SetBinContent(j,1);
+      }
+
+      TGraphErrors *gError = new TGraphErrors(backgroundH[i]);
+
+      gStyle->SetOptStat(0);
 
       dataH[i]->Sumw2();
 
@@ -194,12 +252,84 @@ int plotVar()
 
       TLegend * legenda = gPad->BuildLegend(0.895,0.69,0.65,0.89,"NDC");
 
+      if(vvariable.size()!=1)
+	      c1->cd(i+1);
+      else
+	      c1->cd();
+
+
+      backgroundH[i]->SetMaximum(ratioH[i]->GetMaximum()*1.1);
+      backgroundH[i]->SetMinimum(ratioH[i]->GetMinimum()*0.9);
+
+      TLine *line1 = new TLine(vvariable[i].GetXMin(),1,vvariable[i].GetXMax(),1);
+
+
+      gPad->cd(2);
+      //emptyH[i]->Draw();
+      backgroundH[i]->Draw();
+      gError->Draw("3");
+
+      ratioH[i]->Draw("E same");
+
+      line1->Draw("same");
+      //gError->Draw("a4 same");
+      gError->SetFillColor(6);
+      gError->SetFillStyle(3144);
+      gError->GetYaxis()->SetNdivisions(10);
+      backgroundH[i]->GetYaxis()->SetLabelSize(0.15);
+      backgroundH[i]->GetXaxis()->SetLabelSize(0.15);
+      backgroundH[i]->GetYaxis()->SetTitle("Data/ #Sigma MC");
+      backgroundH[i]->GetYaxis()->SetTitleSize(0.15);
+      backgroundH[i]->GetYaxis()->SetTitleOffset(0.15);
+      gError->GetXaxis()->SetRangeUser(vvariable[i].GetXMin(),vvariable[i].GetXMax());
+
+
+      // Divide Canvas c2
+
       c2->cd();
+      gPad->Divide(1,2);
+      gPad->cd(2);
+      gPad->SetPad(xlow2,ylow2,xup2,yup2);
+
+      c2->cd();
+      gPad->cd(1);
+      gPad->SetPad(xlow1,ylow1,xup1,yup1);
+
+      // Draw in Canvas c2 - Pad1
+
+      gPad->SetLogy(vvariable[i].GetLogy());
+
       Stack->Draw("HIST goff");
       stopH[i]->Draw("HIST same goff");
       dataH[i]->Draw("E same goff");
 
-      TLegend * legenda2 = c2->BuildLegend(0.895,0.69,0.65,0.89,"NDC");
+      TLegend * legenda2 = gPad->BuildLegend(0.895,0.69,0.65,0.89,"NDC");
+
+      //Draw in Canvas c2 - Pad2
+      c2->cd();
+      gPad->cd(2);
+
+      gStyle->SetOptStat(0);
+
+      backgroundH[i]->Reset("ICE");
+      backgroundH[i]->SetTitle(";;Data/ #Sigma MC");
+
+      backgroundH[i]->Draw();
+      gError->Draw("3");
+
+      ratioH[i]->Draw("E same");
+
+      line1->Draw("same");
+      //gError->Draw("a4 same");
+      gError->SetFillColor(6);
+      gError->SetFillStyle(3144);
+      gError->GetYaxis()->SetNdivisions(10);
+      backgroundH[i]->GetYaxis()->SetLabelSize(0.15);
+      backgroundH[i]->GetXaxis()->SetLabelSize(0.15);
+      backgroundH[i]->GetYaxis()->SetTitleSize(0.15);
+      backgroundH[i]->GetYaxis()->SetTitleOffset(0.15);
+      gError->GetXaxis()->SetRangeUser(vvariable[i].GetXMin(),vvariable[i].GetXMax());
+
 
       // Save individual plots as .pdf and .C
       c2->SaveAs(("plots/"+vvariable[i].GetName()+".pdf").c_str());
