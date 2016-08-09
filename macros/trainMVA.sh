739f93d18723bfc921f74f1cfb7b3551ff3ce2f4
bruno <brucms16@lip.pt>
2016-08-09 10:42:40 +0100

    nTuples novos trainMVA.sh

--- macros/trainMVA.sh
+++ macros/trainMVA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SIGNAL=~cbeiraod/local-area/Stop4Body/nTuples_v2016-07-12/T2DegStop_300_270_bdt.root
-BACKGROUND=~cbeiraod/local-area/Stop4Body/nTuples_v2016-07-12/Background_bdt.root
+SIGNAL=~cbeiraod/local-area/Stop4Body/LepFix/T2DegStop_300_270_bdt.root
+BACKGROUND=~cbeiraod/local-area/Stop4Body/LepFix/Background_bdt.root
 
 if [[ -f ${SIGNAL} && -f ${BACKGROUND} ]] ; then
   trainMVA --signalFile ${SIGNAL} --backgroundFile ${BACKGROUND} --nTree 350
